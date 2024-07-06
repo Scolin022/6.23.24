91b1c6889378cdc33d0d52cc576d5c742f851c86
Samantha Colin <samantha.colin022@gmail.com>
2024-07-06 00:02:31 -0500

    html build-in validations added to form

--- public/js/validation.js
+++ public/js/validation.js
@@ -1,15 +1,15 @@
 function validateForm(event) {
     event.preventDefault();
-    let user_name = document.getElementById('user_name').value;
-    let user_email = document.getElementById('user_email').value;
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
     let isValid = true;
 
-    if (user_name.length < 2) {
+    if (name.length < 2) {
         alert('Name must be at least 2 characters long');
         isValid = false;
     }
 
-    if (!user_email.includes('@')) {
+    if (!email.includes('@')) {
         alert('Please enter a valid email address');
         isValid = false;
     }
