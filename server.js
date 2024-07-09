541da25c576bcc8e57169ebf3749faeba68bb295
Samantha Colin <samantha.colin022@gmail.com>
2024-07-08 22:33:40 -0500

    reformatting

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const routes = require('./routes/index');
+const routes = require('./routes/index');ds
 
 app.set('view engine', 'ejs');
 
