78866a83fc8b8a0202eda269304b921346495d6b
Samantha Colin <samantha.colin022@gmail.com>
2024-07-09 22:54:55 -0500

    create dynamic header partial

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+router.use((req, res, next) => {
+    res.locals.path = req.path;
+    next();
+});
+
 router.get('/', (req, res) => {
-    res.render('index', { title: 'Home' });
+    res.render('index', { title: 'Home', path: req.path });
 });
 
 router.get('/about', (req, res) => {
-    res.render('about', { title: 'About Us' });
+    res.render('about', { title: 'About Us', path: req.path });
 });
 
 router.get('/contact', (req, res) => {
-    res.render('contact', { title: 'Contact Us' });
+    res.render('contact', { title: 'Contact Us', path: req.path });
 });
 
 router.get('/services', (req, res) => {
-    res.render('services', { title: 'Our Services' });
+    res.render('services', { title: 'Our Services', path: req.path });
 });
 
 module.exports = router;
